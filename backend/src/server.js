bd6414767bf4bc3ad365cbe120f86c026ba65057
abhishek61067 <abhishek.bhattarai61067@gmail.com>
2025-06-26 14:57:18 +0545

    optimization

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { createServer } from "http";
 import colors from "colors";
-import { initDB, sql } from "./config/db.js";
+import { initDB, sql } from "./src/config/db.js";
 import "dotenv/config";
-import rateLimiter from "./middleware/rateLimiter.js";
+import rateLimiter from "./src/middleware/rateLimiter.js";
 // Import routes
-import transactionRoutes from "./routes/transactionRoutes.js";
+import transactionRoutes from "./src/routes/transactionRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 
